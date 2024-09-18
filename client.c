3cbf4fcb6fea5dfba1737691adddbd46e0fb1e0f
Israel Izquierdo Garcia <iizquier@c3r6s3.42malaga.com>
2024-09-18 17:07:31 +0200

    client

--- client.c
+++ client.c
@@ -6,7 +6,51 @@
 /*   By: iizquier <iizquier@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 13:18:55 by iizquier          #+#    #+#             */
-/*   Updated: 2024/09/14 13:22:12 by iizquier         ###   ########.fr       */
+/*   Updated: 2024/09/18 17:07:02 by iizquier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+static void ft_char_to_bit (const int pid, char c)
+{
+	int mybit;
+
+	mybit = 7;
+	while (mybit >= 0)
+	{
+		if (((c >> mybit) & 1) == 1)
+			kill (pid, SIGUSR1);
+		else 
+			kill (pid, SIGUSR2);
+	}
+	mybit--;
+	usleep (200);
+}
+
+static void ft_check_pid (int pid)
+{
+	if (pid <= 0)
+	{
+		ft_printf (MSG_PID_2 "\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main (int argc, char **argv)
+{
+	int pid;
+	int i;
+	
+	if (argc <= 2)
+	{
+		ft_printf (MSG_ARG_1 "\n");
+		return (1);
+	}
+	if (argc >= 4)
+	{
+		ft_printf (MSG_ARG_2 "\n");
+		return (1);
+	}
+	
+}
